65300d6d13ff9b928fd0fd34a7e05af84a22b391
Unknown <osinov.maks@gmail.com>
2019-02-10 18:57:08 +0200

    upd
    create class Settings and used it into alien_invasion file.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,23 @@
-import sys
-
 import pygame
+
+from settings import Settings
 
 def run_game():
     #Иницилизация игры и создание объекта окна
     pygame.init()
-    screen = pygame.display.set_mode((1200, 900))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+
     pygame.display.set_caption("Alien Invasion")
 
     while True:
         #Отслеживание событий с клавиатуры и мыши
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-        
+                return
+
+        screen.fill(ai_settings.bg_color)
         #Отображение последнего прорисованого экрана
         pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    #Класс для хранения всех настроек игры
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 900
+        self.bg_color = (67, 27, 140)
